94cdcbe6577a632c7b65e49303b937e2e857dc91
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-09-02 22:49:00 +0200

    scripts: clean-{cutie,phosh}: add workaround to get tagged releases upgradeable without droidian-update-service
    This is suboptimal, but can be helpful.
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- scripts/clean-phosh.sh
+++ scripts/clean-phosh.sh
@@ -6,4 +6,9 @@ if [ -f "/usr/bin/flatpak" ]; then
    flatpak remote-modify --enable flathub
 fi
 
+# Workaround until droidian-update-service is in the archive
+if ! grep -q next /etc/apt/apt.conf.d/90-droidian-snapshot; then
+  rm -f /etc/apt/apt.conf.d/90-droidian-snapshot
+fi
+
 exit 0
